de717d0c7fea6dffeaed4f0d88718523701c0fcf
BingChunMoLi <1210509625@qq.com>
2021-10-23 19:49:48 +0800

    feat: springboot升级2.5.6 feat: 引入spring-retry和aspectjweaver feat: enable Async Retry AspectJAutoProxu

--- src/main/java/com/bingchunmoli/api/ApiApplication.java
+++ src/main/java/com/bingchunmoli/api/ApiApplication.java
@@ -4,13 +4,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author BingChunMoLi
  */
+@EnableAsync
+@EnableRetry
 @EnableCaching
 @EnableScheduling
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @MapperScan("com.bingchunmoli.**.mapper")
 public class ApiApplication {
