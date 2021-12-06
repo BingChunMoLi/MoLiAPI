4ac1a4b424153863e6923382092212a0d94eb3af
BingChunMoLi <bingchunmoli@bingchunmoli.com>
2021-12-06 12:04:10 +0800

    feat(新增server酱和和风天气配置项): 生成元数据，新增配置项

---
+++ src/main/java/com/bingchunmoli/api/properties/ApiKeyProperties.java
@@ -0,0 +1,27 @@
+package com.bingchunmoli.api.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author bingchunmoli
+ */
+@Data
+@Component
+@ConfigurationProperties("moli.api-key")
+public class ApiKeyProperties {
+    /**
+     *  和风天气key
+     */
+    private String weatherKey;
+    /**
+     * 和风天气请求uri
+     */
+    private String weatherUri = "devapi.qweather.com";
+    /**
+     * server酱key
+     */
+    private String serverSauceKey;
+
+}
