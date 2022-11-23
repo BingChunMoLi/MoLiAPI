87c40846c30afcb8783d289ff8ae85b867a158d4
BingChunMoLi <bingchunmoli@bingchunmoli.com>
2022-11-23 21:26:26 +0800

    build(3.10.1): init

--- src/test/java/com/bingchunmoli/api/ApiApplicationTests.java
+++ src/test/java/com/bingchunmoli/api/ApiApplicationTests.java
@@ -2,10 +2,18 @@ package com.bingchunmoli.api;
 
 import com.bingchunmoli.api.properties.ApiConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Slf4j
 @SpringBootTest
 class ApiApplicationTests {
@@ -16,6 +24,17 @@ class ApiApplicationTests {
     void before(){
         if (log.isDebugEnabled()) {
             log.debug("apiKeyProperties: {}", apiConfig);
+        }
+    }
+
+    @Value("${moli.path}")
+    private String initPath;
+
+    @BeforeAll
+    void init() throws IOException {
+        Path path = Paths.get(initPath);
+        if (!path.toFile().exists()) {
+            Files.createDirectories(path);
         }
     }
 
