1b0f787d1b3ca1855f858c0b22fe5b8daebbc82f
bingchunmoli <44092516+BingChunMoLi@users.noreply.github.com>
2024-01-30 18:53:05 +0800

    fix(3.15.1): JSR303 验证异常, LocalDate时间格式化问题, 异常消息推送错误 JSR303 https://stackoverflow.com/questions/77545551/error-illegalstateexception-no-target-validator-set-after-upgrade-from-spring feat: test
    [skip ci]

--- src/test/java/com/bingchunmoli/api/bing/controller/BingControllerTest.java
+++ src/test/java/com/bingchunmoli/api/bing/controller/BingControllerTest.java
@@ -1,66 +1,104 @@
 package com.bingchunmoli.api.bing.controller;
 
+import com.baomidou.mybatisplus.test.autoconfigure.AutoConfigureMybatisPlus;
+import com.bingchunmoli.api.app.DeviceService;
+import com.bingchunmoli.api.app.mapper.DeviceMapper;
 import com.bingchunmoli.api.bing.bean.BingImage;
 import com.bingchunmoli.api.bing.bean.BingImageVO;
 import com.bingchunmoli.api.bing.bean.enums.BingEnum;
 import com.bingchunmoli.api.bing.mapper.BingImageMapper;
 import com.bingchunmoli.api.bing.service.BingService;
-import com.bingchunmoli.api.bing.service.impl.BingServiceImpl;
 import com.bingchunmoli.api.utils.RedisUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.util.Assert;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
-@JsonTest
-@ContextConfiguration(classes = {BingService.class, BingServiceImpl.class})
+@AutoConfigureMybatisPlus
+@WebMvcTest(BingController.class)
 public class BingControllerTest {
 
     @Autowired
-    private ObjectMapper om;
-    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
     private BingService bingService;
     @MockBean
-    private RedisUtil redisUtil;
+    private BingImageMapper bingMapper;
     @MockBean
-    private BingImageMapper bingImageMapper;
-    private BingImageVO cnBingImage;
-    private BingImageVO enBingImage;
+    private DeviceService deviceService;
+    @MockBean
+    private DeviceMapper deviceMapper;
+    @MockBean
+    private RedisUtil redisUtil;
 
     @Test
-    void getBingCnTest() {
-        cnBingImage = bingService.getBingImageByRemote(BingEnum.CN_BING);
-        Assert.notNull(cnBingImage, "cn.bing.com not get random image");
+    void cnBingImage() throws Exception {
+        Mockito.when(bingService.getBingImage(BingEnum.CN_BING)).thenReturn(new BingImageVO());
+        mockMvc.perform(MockMvcRequestBuilders.get("/bing/cn"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getBingImage(BingEnum.CN_BING);
     }
 
     @Test
-    void getBingEnTest() {
-        enBingImage = bingService.getBingImageByRemote(BingEnum.EN_BING);
-        Assert.notNull(enBingImage, "bing.com not get random image");
+    void enBingImage() throws Exception {
+        Mockito.when(bingService.getBingImage(BingEnum.EN_BING)).thenReturn(new BingImageVO());
+        mockMvc.perform(MockMvcRequestBuilders.get("/bing/en"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getBingImage(BingEnum.EN_BING);
     }
 
     @Test
-    void getBingAllTest() {
-        cnBingImage = bingService.getBingImageByRemote(BingEnum.CN_BING);
-        enBingImage = bingService.getBingImageByRemote(BingEnum.EN_BING);
-        BingImage bingImage = new BingImage(cnBingImage, enBingImage);
-        Assert.isTrue(LocalDate.parse(bingImage.getEndDate(), DateTimeFormatter.ofPattern("yyyyMMdd")).isEqual(LocalDate.now()), "获取的bingImage.EndDate已过时");
-        Assert.isTrue(bingImage.getCreateTime().toLocalDate().isEqual(LocalDate.now()), "获取的bingImage.creatTime已过时");
+    void getAllBingImage() throws Exception {
+        Mockito.when(bingService.getAllBingImage()).thenReturn(new BingImage());
+        mockMvc.perform(MockMvcRequestBuilders.get("/bing/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getAllBingImage();
     }
 
     @Test
-    void jsonParse() throws JsonProcessingException {
-        String s = "{\"images\":[{\"startdate\":\"20211007\",\"fullstartdate\":\"202110071600\",\"enddate\":\"20211008\",\"url\":\"/th?id=OHR.FriendlyOctopus_ZH-CN2519447724_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp\",\"urlbase\":\"/th?id=OHR.FriendlyOctopus_ZH-CN2519447724\",\"copyright\":\"海草中的章鱼，法国利翁湾 (© BIOSPHOTO/Alamy)\",\"copyrightlink\":\"https://www.bing.com/search?q=%E7%AB%A0%E9%B1%BC&form=hpcapt&mkt=zh-cn\",\"title\":\"\",\"quiz\":\"/search?q=Bing+homepage+quiz&filters=WQOskey:%22HPQuiz_20211007_FriendlyOctopus%22&FORM=HPQUIZ\",\"wp\":true,\"hsh\":\"2e4cdcc79efcd8dd814ce96c12f19125\",\"drk\":1,\"top\":1,\"bot\":1,\"hs\":[]}],\"tooltips\":{\"loading\":\"正在加载...\",\"previous\":\"上一个图像\",\"next\":\"下一个图像\",\"walle\":\"此图片不能下载用作壁纸。\",\"walls\":\"下载今日美图。仅限用作桌面壁纸。\"}}\n";
-        BingImageVO vo = om.readValue(s, BingImageVO.class);
-        Assert.notNull(vo, "bingImageJson解析失败");
+    void getRandomBingImg() throws Exception {
+        Mockito.when(bingService.getRandomImg()).thenReturn("");
+        mockMvc.perform(MockMvcRequestBuilders.get("/bing/random"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getRandomImg();
+    }
+
+    @Test
+    void getImageByYearMonthDay() throws Exception {
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int month = now.getMonth().getValue();
+        int day = now.getDayOfMonth();
+        Mockito.when(bingService.getBingImageByDate(now)).thenReturn(new BingImage());
+        mockMvc.perform(MockMvcRequestBuilders.get("""
+                /bing/{year}/{month}/{day}
+                """.strip(), year, month, day))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getBingImageByDate(now);
+    }
+
+    @Test
+    void getBingImageByDate() throws Exception {
+        LocalDate now = LocalDate.now();
+        Mockito.when(bingService.getBingImageByDate(now)).thenReturn(new BingImage());
+        mockMvc.perform(MockMvcRequestBuilders.get("/bing/date?date=" + now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        Mockito.verify(bingService, Mockito.times(1)).getBingImageByDate(now);
     }
 }
