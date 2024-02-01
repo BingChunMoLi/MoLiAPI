11976625fedf2963b228ec9a874cddca1ac3dc2e
bingchunmoli <44092516+BingChunMoLi@users.noreply.github.com>
2024-02-01 19:08:39 +0800

    feat(3.15.2): config

--- src/main/vue/admin/components.d.ts
+++ src/main/vue/admin/components.d.ts
@@ -6,18 +6,13 @@
 export {}
 
 declare module 'vue' {
-    export interface GlobalComponents {
-        ElAlert: typeof import('element-plus/es')['ElAlert']
-        ElButton: typeof import('element-plus/es')['ElButton']
-        ElForm: typeof import('element-plus/es')['ElForm']
-        ElFormItem: typeof import('element-plus/es')['ElFormItem']
-        ElInput: typeof import('element-plus/es')['ElInput']
-        IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-        IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-        IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-        IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-        IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
-        RouterLink: typeof import('vue-router')['RouterLink']
-        RouterView: typeof import('vue-router')['RouterView']
-    }
+  export interface GlobalComponents {
+    ElAlert: typeof import('element-plus/es')['ElAlert']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
 }
