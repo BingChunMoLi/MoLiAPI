74ff64e36953c23f102f05ffcb4c0e485c9fd71a
BingChunMoLi <44092516+BingChunMoLi@users.noreply.github.com>
2024-08-04 22:26:26 +0800

    feat: 网易云

--- src/main/java/com/bingchunmoli/api/netease/MusicController.java
+++ src/main/java/com/bingchunmoli/api/netease/MusicController.java
@@ -1,9 +1,11 @@
 package com.bingchunmoli.api.netease;
 
+import com.bingchunmoli.api.bean.ResultVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,8 @@ public class MusicController {
         return musicService.getPlayListInfo(id, cookie);
     }
 
+    @GetMapping("{id}")
+    public ResultVO<String> getMusicLink(@PathVariable String id){
+        return ResultVO.ok("https://music.163.com/api/playlist/detail?id=" + id + "&offset=0&total=true&limit=1001");
+    }
 }
