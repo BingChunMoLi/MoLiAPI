5d64baf09e7fd3cf146a5079f1947524d7b04bb4
BingChunMoLi <44092516+BingChunMoLi@users.noreply.github.com>
2024-09-22 18:18:48 +0800

    fix(3.15.9): pwa

--- src/main/resources/static/pwa.js
+++ src/main/resources/static/pwa.js
@@ -54,7 +54,7 @@ self.addEventListener('activate', event => {
  */
 self.addEventListener('fetch', event => {
   // Skip some of cross-origin requests, like those for Google Analytics.
-  if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+  if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1 && event.request.method.toUpperCase() === 'GET') {
     // Stale-while-revalidate
     // similar to HTTP's stale-while-revalidate: https://www.mnot.net/blog/2007/12/12/stale
     // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
