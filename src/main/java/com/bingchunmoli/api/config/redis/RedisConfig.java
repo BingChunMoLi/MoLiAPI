dd6d99d4807c3c3b9ccfe5114b8e0a4267615180
BingChunMoLi <44092516+BingChunMoLi@users.noreply.github.com>
2025-12-16 22:58:21 +0800

    fix: redisSerializer

--- src/main/java/com/bingchunmoli/api/config/redis/RedisConfig.java
+++ src/main/java/com/bingchunmoli/api/config/redis/RedisConfig.java
@@ -1,16 +1,19 @@
 package com.bingchunmoli.api.config.redis;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.DefaultTyping;
 import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 /**
  * Redis序列化配置类
@@ -39,8 +42,13 @@ public class RedisConfig implements CachingConfigurer {
     @Bean
     public RedisSerializer<Object> getRedisSerializer() {
         JsonMapper mapper = JsonMapper.builder()
+                .activateDefaultTyping(BasicPolymorphicTypeValidator.builder()
+                                .allowIfBaseType(Object.class)
+                                .build(),
+                        DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.PROPERTY)
                 .build();
-        return new JacksonJsonRedisSerializer<>(mapper, Object.class);
+        return new GenericJacksonJsonRedisSerializer(mapper);
     }
 
 }
